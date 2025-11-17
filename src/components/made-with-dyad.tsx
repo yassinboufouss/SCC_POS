002883ea4b9ed3346a9113b46106611f60ec8b61
[dyad] <git@dyad.sh>
2025-11-17 21:19:24 +0100

    Updated src/components/made-with-dyad.tsx

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -7,7 +7,7 @@ export const MadeWithDyad = () => {
         rel="noopener noreferrer"
         className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
       >
-        Made with Dyad
+        Create by Manga
       </a>
     </div>
   );
