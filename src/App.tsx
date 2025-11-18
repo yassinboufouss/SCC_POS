87b6f4e72c970a212456845fb7fb4d1536947bfc
[dyad] <git@dyad.sh>
2025-11-18 03:30:41 +0100

    [dyad] Removed unused files and consolidated the toast system. - wrote 1 file(s), deleted 9 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -9,7 +8,6 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <Toaster />
       <Sonner />
       <POSPage />
     </TooltipProvider>
