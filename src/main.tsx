87ea6b968275b7c0d08c3218a719744d7a495ab6
[dyad] <git@dyad.sh>
2025-11-18 21:19:13 +0100

    [dyad] Added React Suspense wrapper to fix potential white screen issue during initialization. - wrote 2 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import "./i18n.ts"; // Import i18n setup
+import React, { Suspense } from "react";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Suspense fallback={<div>Loading application...</div>}>
+      <App />
+    </Suspense>
+  </React.StrictMode>
+);
